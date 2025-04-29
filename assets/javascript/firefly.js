fa7613a3d328df777c37412e2f385ebf1f3bd6cb
hybridrev <rafazil.crafter330@gmail.com>
2025-04-29 08:01:32 +0700

    Updated

--- assets/javascript/firefly.js
+++ assets/javascript/firefly.js
@@ -1,4 +1,4 @@
-const lines = ["Kamu adalah cahaya paling bersinar di dunia untukku. Kamu telah menerangi hatiku yang dulu sepi. Sejak bertemu denganmu, duniaku berubah menjadi lebih hangat dan lembut.", "Melihatmu saja sudah membuat hati ini terasa senang. Aku bahkan tidak menyangka bisa bertemu denganmu saat itu.", "Aku tidak tau apa yang terjadi kalo aku tidak pernah bertemu denganmu saat itu, mungkin aku akan tetap seperti dulu.", "Kau telah mengubah diriku sejauh ini, meskipun kau tidak tau akan hal itu.", "Tapi aku hanya ingin kau tau, bahwa senyum manismu telah menciptakan \"Butterfly Effect\" pada hidupku."];
+const lines = "Makan mie ayam enak kali yak";
 const output = document.getElementById("output");
 
 lines.forEach(line => {
