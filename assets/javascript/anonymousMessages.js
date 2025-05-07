5a097b810d849d2e81a4010b83f6a23a213e5eca
hybridrev <rafazil.crafter330@gmail.com>
2025-05-07 20:01:28 +0700

    Updated

---
+++ assets/javascript/anonymousMessages.js
@@ -0,0 +1,35 @@
+document.querySelector("form").addEventListener("submit", function (e) {
+    const textarea = this.querySelector("textarea");
+    const button = this.querySelector("button");
+  
+    if (textarea.value.trim().length === 0) {
+      e.preventDefault();
+      const originalText = button.textContent;
+      button.textContent = "Pesan tidak boleh kosong!";
+      button.disabled = true;
+  
+      setTimeout(() => {
+        button.textContent = originalText;
+        button.disabled = false;
+      }, 3000);
+    }
+  });
+  
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("countdown-text");
+  
+    let timeLeft = 10;
+    button.disabled = true;
+    button.textContent = `Kirim (${timeLeft})`;
+  
+    const timer = setInterval(() => {
+      timeLeft--;
+      if (timeLeft > 0) {
+        button.textContent = `Kirim (${timeLeft})`;
+      } else {
+        clearInterval(timer);
+        button.disabled = false;
+        button.textContent = "Kirim";
+      }
+    }, 1000);
+  });
